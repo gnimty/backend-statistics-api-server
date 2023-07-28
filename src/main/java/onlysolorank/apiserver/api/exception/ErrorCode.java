f22d753ed041ea6a51aab8a9fefe465a0a4369c4
so1omon <solmin3665@gmail.com>
2023-07-28 16:15:48 +0900

    feat: 소환사 이름 자동완성 결과 조회 API 에러처리 및 로직 수정

--- src/main/java/onlysolorank/apiserver/api/exception/ErrorCode.java
+++ src/main/java/onlysolorank/apiserver/api/exception/ErrorCode.java
@@ -14,13 +14,15 @@ import org.springframework.http.HttpStatus;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2023/07/10        solmin       최초 생성
+ * 2023/07/28        solmin       CONSTRAINT_VIOLATION 추가
  */
 
 @Getter
 public enum ErrorCode {
     // Error Code 정의
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Invalid Input Value"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Server Error");
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Input Value Erroer"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Server Error"),
+    CONSTRAINT_VIOLATION(HttpStatus.BAD_REQUEST, "입력값 중 검증에 실패한 값이 있습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/onlysolorank/apiserver/api/handler/GlobalExceptionHandler.java
+++ src/main/java/onlysolorank/apiserver/api/handler/GlobalExceptionHandler.java
@@ -1,11 +1,22 @@
 package onlysolorank.apiserver.api.handler;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import onlysolorank.apiserver.api.exception.CustomException;
+import onlysolorank.apiserver.api.exception.ErrorCode;
 import onlysolorank.apiserver.api.response.CommonResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * packageName    : onlysolorank.apiserver.api.controller
@@ -17,12 +28,65 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2023/07/10        solmin       최초 생성
+ * 2023/07/28        solmin       BindException, CustomException, ConstraintViolationException 공통 에러 처리
  */
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    // 공통 Exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponse> CommonExceptionHandler(Exception ex, BindingResult bindingResult) {
+        return CommonResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<CommonResponse> handleException(CustomException ex) {
+    public ResponseEntity<CommonResponse> CustomExceptionHandler(CustomException ex) {
         return CommonResponse.fail(ex.getErrorCode());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<CommonResponse<List<ValidateError>>> BindExceptionHandler(BindException ex, BindingResult result) {
+
+        List<ValidateError> validateErrors = result.getFieldErrors().stream()
+            .map(e -> ValidateError.builder()
+                .field(e.getField())
+                .message(e.getDefaultMessage())
+                .build()
+            ).collect(Collectors.toList());
+
+        return CommonResponse.fail(ErrorCode.CONSTRAINT_VIOLATION, validateErrors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<CommonResponse<List<ValidateError>>> ConstraintViolationExceptionHandler(ConstraintViolationException ex) {
+        List<ValidateError> validateErrors= new ArrayList<>();
+
+        ex.getConstraintViolations()
+            .stream()
+            .forEach(constraintViolation -> {
+                validateErrors.add(ValidateError.builder()
+                    .propertypath(constraintViolation.getPropertyPath())
+                    .message(constraintViolation.getMessage())
+                    .build());
+            });
+
+        return CommonResponse.fail(ErrorCode.CONSTRAINT_VIOLATION, validateErrors);
+    }
+
+    @Data
+    private static class ValidateError{
+        private String field;
+        private String message;
+
+        @Builder
+        public ValidateError(Path propertypath, String field, String message){
+            if (field ==null){
+                this.field =  Arrays.stream(propertypath.toString().split("\\.")).reduce((first, second) -> second).orElse("none");
+            }else{
+                this.field = field;
+            }
+            this.message = message;
+        }
     }
 }
