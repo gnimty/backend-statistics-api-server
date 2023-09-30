c971400c1166603758d7300eb438dfa6b00176a1
so1omon <solmin3665@gmail.com>
2023-09-30 17:59:54 +0900

    feat: 챔피언 통계정보, 포지션별 챔피언 티어정보 개발 -> 리팩토링 필요

---
+++ src/main/java/onlysolorank/apiserver/api/service/dto/ChampionStatBriefDto.java
@@ -0,0 +1,40 @@
+package onlysolorank.apiserver.api.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import onlysolorank.apiserver.domain.statistics.BaseChampionStat;
+
+/**
+ * packageName    : onlysolorank.apiserver.api.service.dto
+ * fileName       : ChampionStatsBriefDto
+ * author         : solmin
+ * date           : 2023/09/30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/09/30        solmin       최초 생성
+ */
+
+@Data
+@AllArgsConstructor
+public class ChampionStatBriefDto {
+    private Long championId;
+//    private Position position;
+    private Long plays;
+    private Long wins;
+    private Long defeats;
+    private Long bans;
+
+    @Builder
+    public ChampionStatBriefDto(BaseChampionStat stats){
+        this.championId = stats.getChampionId();
+//        this.position = stats.getTeamPosition();
+        this.plays = stats.getPlays();
+        this.wins = stats.getWins();
+        this.defeats = plays - wins;
+        this.bans = stats.getBans();
+    }
+
+}
---
+++ src/main/java/onlysolorank/apiserver/domain/ChampionStatsDetail.java
@@ -0,0 +1,17 @@
+package onlysolorank.apiserver.domain;
+
+/**
+ * packageName    : onlysolorank.apiserver.domain
+ * fileName       : ChampionStatsDetail
+ * author         : solmin
+ * date           : 2023/09/30
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/09/30        solmin       최초 생성
+ */
+
+
+public class ChampionStatsDetail {
+}
