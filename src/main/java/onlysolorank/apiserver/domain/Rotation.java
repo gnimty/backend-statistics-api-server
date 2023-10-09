94d648fdc544f7cabf9ea43180dd013f87ed45a1
so1omon <37677461+so1omon@users.noreply.github.com>
2023-10-09 15:47:15 +0900

    Feature/#5 crawl api update (#44)
    * feat: 크롤링 API 업데이트 및 로테이션 챔피언 API 추가
    
    * fix: untracked file 추가

---
+++ src/main/java/onlysolorank/apiserver/domain/Rotation.java
@@ -0,0 +1,28 @@
+package onlysolorank.apiserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * packageName    : onlysolorank.apiserver.domain
+ * fileName       : Rotation
+ * author         : solmin
+ * date           : 10/9/23
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 10/9/23        solmin       최초 생성
+ */
+
+@Document(collection = "rotations")
+@Getter
+public class Rotation extends Champion{
+
+    public Rotation(String id, Long championId, String krName, String enName) {
+        super(id, championId, krName, enName);
+    }
+}
